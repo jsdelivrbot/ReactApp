508deef02d386947ebc4f2dc0ee2190b08276a37
Arturo Montoya <montoya332@live.com>
2017-01-28 14:47:59 -0800

    Modified : devtool source map

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,5 @@
 import React, { Component} from 'react';
-import HeaderBar from './header/header'
+import HeaderBar from './header/headerBar'
 export default class App extends Component {
     render() {
         return ( <HeaderBar title="Title" iconClassNameRight="muidocs-icon-navigation-expand-more" />  );
